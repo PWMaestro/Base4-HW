b5e7a88e1bd7f68bb9d1dbd79f114bace1d66446
Artiom Sitko <artiom.sitko@gmail.com>
2021-03-11 08:58:53 +0300

    Task 3. Names of variables changed

--- task-3/task-3.cpp
+++ task-3/task-3.cpp
@@ -6,24 +6,24 @@ using namespace std;
 
 int main()
 {
-    int sum = 0, r = 0, n;
+    int sum = 0, decimalNumber = 0, binaryLength;
     char binary[255];
 
     cout << "Enter binary number: ";
     cin >> binary;
 
-    n = strlen(binary);
-    int arr[n];
+    binaryLength = strlen(binary);
+    int arr[binaryLength];
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < binaryLength; i++) {
         arr[i] = static_cast<int>( binary[i] ) - static_cast<int>( '0' );
         if (arr[i] != 0 && arr[i] != 1) {
             cout << "Error! Wrong number system!" << endl;
             return 1;
         }
-        r += arr[i] * pow( 2, n - i - 1 );
+        decimalNumber += arr[i] * pow( 2, binaryLength - i - 1 );
     }
 
-    cout << "Decimal binary is equal: " << r;
+    cout << "Equivalent decimal is: " << decimalNumber;
     return 0;
 }
