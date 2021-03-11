0e7a5a79dc1aadc8fb0b6c2d98af0838614992cd
Artiom Sitko <artiom.sitko@gmail.com>
2021-03-11 08:52:47 +0300

    Task 2. Code refactoring with changing names of variables

--- task-2/task-2.cpp
+++ task-2/task-2.cpp
@@ -5,22 +5,31 @@ using namespace std;
 
 int main()
 {
-    int x1 = 0, x2 = 0, y1 = 0, y2 = 0;
+    //int x1 = 0, x2 = 0, y1 = 0, y2 = 0;
+    int playgroundDimensionX = 8,
+        playgroundDimensionY = 8,
+        knigthCoordinatesX[2],
+        knigthCoordinatesY[2];
 
     cout << "Enter the coordinates:" << endl;
     cout << "Coordinate X of first knight: ";
-    cin >> x1;
+    cin >> knigthCoordinatesX[0];
     cout << "Coordinate Y of first knight: ";
-    cin >> y1;
+    cin >> knigthCoordinatesY[0];
     cout << "Coordinate X of second knight: ";
-    cin >> x2;
+    cin >> knigthCoordinatesX[1];
     cout << "Coordinate Y of second knight: ";
-    cin >> y2;
+    cin >> knigthCoordinatesY[1];
 
-    if ( x1 > 8 || x2 > 8 || y1 > 8 || y2 > 8 || x1 < 1 || x2 < 1 || y1 < 1 || y2 < 1) {
-        cout << "Error! Wrong coordinates!";
-        return 1;
-    } else if (( abs(x1 - x2) == 2 && abs(y1 - y2) == 1 ) || ( abs(x1 - x2) == 1 && abs(y1 - y2) == 2 )) {
+    for (int i = 0; i < 2; i++) {
+        if ( knigthCoordinatesX[i] < 1 || knigthCoordinatesX[i] > 8 || knigthCoordinatesY[i] < 1 || knigthCoordinatesY[i] > 8 ) {
+            cout << "Error! Wrong coordinates!";
+            return 1;
+        }
+    } 
+
+    if (( abs(knigthCoordinatesX[0] - knigthCoordinatesX[1]) == 2 && abs(knigthCoordinatesY[0] - knigthCoordinatesY[1]) == 1 ) ||
+        ( abs(knigthCoordinatesX[0] - knigthCoordinatesX[1]) == 1 && abs(knigthCoordinatesY[0] - knigthCoordinatesY[1]) == 2 )) {
         cout << "Knights beat each other." << endl;
     } else {
         cout << "Knights don't beat each other." << endl;
