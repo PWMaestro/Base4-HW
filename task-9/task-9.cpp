8eb23c284fbc58ee2a7461cb090f639a58bb5ad3
Artiom Sitko <artiom.sitko@gmail.com>
2021-03-15 11:35:13 +0300

    Task 9 added

---
+++ task-9/task-9.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int max, currentValue = 1;
+
+    cout << "Enter a maximum number: ";
+    cin >> max;
+
+    if (max <= 0) {
+        cout << "Error! Incorrect input. The number must be greater than zero.";
+        return 1;
+    } else {
+        cout << "Calculated numbers:" << endl;
+        do {
+            cout << currentValue << endl;
+            currentValue = 2 * currentValue + 1;
+        } while (currentValue <= max);   
+    }
+
+    return 0;
+}
