657a465ef2596f691515d51d70b6cd0a4e10d3ec
Artiom Sitko <AC3600@yandex.by>
2021-04-12 17:18:57 +0300

    App frame added
    App is working, but it can't to do anything.

--- task-21/task-21-field-of-dreams.cpp
+++ task-21/task-21-field-of-dreams.cpp
@@ -2,8 +2,60 @@
 
 using namespace std;
 
+void startGame();
+void showMsgWrongInput(string additionalMessage ="");
+
+bool agreeYesOrNo(string message);
+bool agreePlayAgain();
+
 int main()
 {
-    cout << "Hello, world!";
+    do
+    {
+        startGame();
+    } while (agreePlayAgain());
+
     return 0;
+}
+
+
+
+void startGame()
+{
+    cout << "Game is running..." << endl;
+}
+
+void showMsgWrongInput(string additionalMessage/* =""*/)
+{
+    cout << "Wrong input! Try again..." << endl;
+    cout << additionalMessage << endl;
+}
+
+bool agreeYesOrNo(string message)
+{
+    char userInput;
+
+    while (true)
+    {
+        cout << message << endl;
+        cout << "Enter Y / N: ";
+        cin >> userInput;
+
+        switch (userInput)
+        {
+        case 'y':
+            return true;
+        case 'n':
+            return false;
+        
+        default:
+            showMsgWrongInput();
+            break;
+        }
+    }
+}
+
+bool agreePlayAgain()
+{
+    return agreeYesOrNo("Do you want to play again?");
 }
